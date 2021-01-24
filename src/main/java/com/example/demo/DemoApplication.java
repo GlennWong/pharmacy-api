41f8afbc40b6dc2a348ec776df3acc63a7879a65
glen.wang <glen.wang@d8aspring.com>
2021-01-24 17:09:16 +0800

    oauth2 client

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,30 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+//        http
+//            .authorizeRequests(a -> a
+//                .antMatchers("/", "/users", "/**").permitAll()
+//                .anyRequest().authenticated()
+//            )
+//            .exceptionHandling(e -> e
+//                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//            )
+//            .oauth2Login();
+        // @formatter:on
+    }
 }
